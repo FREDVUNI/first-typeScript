7bf6503f7cda1ed7a6c10bf0374e4ceab6ff3fb8
TAATA EARL <fredvuni809@gmail.com>
2022-09-02 13:19:07 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+//functions
+function sum(a: number, b: number):number{
+    return a + b;
+}
+
+console.log(sum(1,3))
+
+//arrays
+let arrayNames:string[] = ["Fred","vuni"]
+
+console.log(arrayNames)
+
+//variables
+let year:number = 2022
+
+let Myname:string = "Fred"
+
+let married:boolean
+married = true
+
